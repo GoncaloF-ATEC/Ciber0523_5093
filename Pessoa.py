a34c15dfdf24f549214545184b753ca53d898c04
GoncaloF-ATEC <Goncalo.Feliciano.0001150@edu.atec.pt>
2023-07-20 18:52:33 +0100

    19h

--- Pessoa.py
+++ Pessoa.py
@@ -1,12 +1,30 @@
 from dataclasses import dataclass
 
 @dataclass
+class Morada:
+    rua: str
+    porta: str
+    apt: str
+
+@dataclass
 class Pessoa:
     nome: str
     idade: int
     email: str
+    morada: Morada
 
 @dataclass
 class Ponto:
     x: int
     y: int
+
+
+class PessoaC:
+    def __init__(self, nome, idade, email):
+        self.nome = nome
+        self.idade = idade
+        self.email = email
+
+    def ano_nasc(self):
+
+        return 2023 - self.idade
--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-from Pessoa import Pessoa, PessoaC
+from Pessoa import Pessoa, PessoaC, Morada
+
+print("--"*10)
+print("--"*10)
 
 #dataclass
 
-p = Pessoa("Gonçalo", 37, "teste@sapo.pt")
-print(p)
+m = Morada("rua 1", '40', "234")
+p = Pessoa("Gonçalo", 37, "teste@sapo.pt", m)
+p2 = Pessoa(
+    nome="Gonçalo",
+    idade=37,
+    email="teste@sapo.pt",
+    morada=Morada("rua 1", '40', "234"))
 
+print(p2)
+'''
+Pessoa(
+    nome='Gonçalo', 
+    idade=37, 
+    email='teste@sapo.pt', 
+    morada=Morada(
+        rua='rua 1', 
+        porta='40', 
+        apt='234'
+    )
+)
+'''
 pc = PessoaC("Gonçalo", 37, "teste@sapo.pt")
 print(pc)
 
